427a82c2608cdc6480cda9a56ed3a7d9339f686e
abhildevkc <abhildevkc7@gmail.com>
2022-09-14 10:13:20 +0530

    tester

---
+++ Fly/src/main/java/com/tcs/fly/model/Login.java
@@ -0,0 +1,31 @@
+/*
+ * package com.tcs.fly.model;
+ * 
+ * import javax.persistence.Entity; import javax.persistence.GeneratedValue;
+ * import javax.persistence.Id; import javax.persistence.Table;
+ * 
+ * import lombok.Getter; import lombok.NoArgsConstructor; import lombok.Setter;
+ * import lombok.ToString;
+ * 
+ * @Entity
+ * 
+ * @Table(name="Login")
+ * 
+ * public class Login { public Login(int id, String name, String password) {
+ * super(); this.id = id; this.name = name; this.password = password; } public
+ * Login() { super(); // TODO Auto-generated constructor stub }
+ * 
+ * @Id
+ * 
+ * @GeneratedValue int id; String name; String password; public int getId() {
+ * return id; } public void setId(int id) { this.id = id; } public String
+ * getName() { return name; } public void setName(String name) { this.name =
+ * name; } public String getPassword() { return password; } public void
+ * setPassword(String password) { this.password = password; }
+ * 
+ * @Override public String toString() { return "Login [id=" + id + ", name=" +
+ * name + ", password=" + password + "]"; }
+ * 
+ * 
+ * }
+ */
